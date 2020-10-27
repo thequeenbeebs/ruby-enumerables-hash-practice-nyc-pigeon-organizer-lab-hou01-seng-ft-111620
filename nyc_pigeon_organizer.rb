6c6a8eb50033be0b725f748048c6e66da719a19a
Blaire Baker <bakerblaire@gmail.com>
2020-10-27 14:09:49 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,12 +1,36 @@
+require 'pry'
+
+pigeon_data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
+
 def nyc_pigeon_organizer(data)
   new_hash = {}
   data.each do |attribute, adjective|
     data[attribute].each do |adjective, array_of_names|
       data[attribute][adjective].each do |array_of_names|
         new_hash[array_of_names] = {}
+        binding.pry
       end
       new_hash[array_of_names][attribute].push(data[attribute])
     end
   end
   new_hash
 end
+
+nyc_pigeon_organizer(pigeon_data)
