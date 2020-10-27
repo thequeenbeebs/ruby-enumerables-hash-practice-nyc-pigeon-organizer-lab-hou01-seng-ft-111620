6bc6cfbe0ffdb6cac87eeac0b36adb960271fa94
Blaire Baker <bakerblaire@gmail.com>
2020-10-27 15:15:07 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -24,7 +24,7 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), final_array|
     value.each do |inner_key, names|
       names.each do |name|
-        final_array[name] = {}
+        final_array[name] = {key => inner_key}
         binding.pry
       end
     end
