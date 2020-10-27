fe8c7215944abf103801f584efd6fd3e23de607c
Blaire Baker <bakerblaire@gmail.com>
2020-10-27 15:13:20 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -22,10 +22,10 @@ pigeon_data = {
 
 def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), final_array|
-    binding.pry
     value.each do |inner_key, names|
       names.each do |name|
         final_array[name] = {}
+        binding.pry
       end
     end
   end
