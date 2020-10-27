b31a418d6e63a9245155aa97c42abde6998aac94
Blaire Baker <bakerblaire@gmail.com>
2020-10-27 14:07:05 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,12 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |attribute, adjective|
+    data[attribute].each do |adjective, array_of_names|
+      data[attribute][adjective].each do |array_of_names|
+        new_hash[array_of_names] = {}
+      end
+      new_hash[array_of_names][attribute].push(data[attribute])
+    end
+  end
+  new_hash
 end
