557be0d4b2439f2f0e4b6338f29a368a918e3c18
Blaire Baker <bakerblaire@gmail.com>
2020-10-27 15:27:01 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -27,6 +27,9 @@ def nyc_pigeon_organizer(data)
         if !final_array[name]
           final_array[name] = {}
         end
+        if !final_array[name][key]
+          final_array[name][key] = []
+        end
       end
       binding.pry
     end
