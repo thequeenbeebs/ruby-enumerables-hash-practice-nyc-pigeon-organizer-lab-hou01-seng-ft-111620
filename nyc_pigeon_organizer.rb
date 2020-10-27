dbf6f21e81a60e7a132de6eb08bc2164dd07da21
Blaire Baker <bakerblaire@gmail.com>
2020-10-27 14:46:46 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -35,6 +35,9 @@ pigeon_data = {
 def nyc_pigeon_organizer(data)
   new_hash = {}
   data.each do |attribute, adjective|
+    data[attribute].each do |adjective, array_of_names|
+      
+    end
     binding.pry
   end
   new_hash
